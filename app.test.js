83282c7ac9cc74491da8d906e8c1464254f65d70
venkateshwork <116881065+venkateshwork@users.noreply.github.com>
2023-02-13 00:29:59 +0530

    Add files via upload

---
+++ app.test.js
@@ -0,0 +1,11 @@
+test('hello world', () => {
+	expect('hello world').toBe('hello world');
+});
+
+test('success', () => {
+	expect('success').toBe('success');
+});
+
+test('failed', () => {
+	expect('failed').toBe('failed');
+});
