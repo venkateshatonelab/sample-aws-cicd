c9cf21e1e73e977f7c9414c2f238beb57bb5a7e1
venkateshwork <116881065+venkateshwork@users.noreply.github.com>
2023-03-05 17:57:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 mongoose
-	.connect(process.env.DATABASE_URI, {
+	.connect(process.env.DATABASE_URI || "mongodb+srv://venkatesh:venkatesh@amplify-test.1eigwf3.mongodb.net/?retryWrites=true&w=majority", {
 		useUnifiedTopology: false,
 	})
 	.then(() => {
